36d02f372d625a1ada8e92557d3a181092cd5f65
Revazashvili <levanrevazashvili@gmail.com>
2021-11-12 11:48:50 +0400

    refactor sdk code

--- sdk/BlobStore.SDK/ServiceCollectionExtension.cs
+++ sdk/BlobStore.SDK/ServiceCollectionExtension.cs
@@ -16,21 +16,27 @@ public static class ServiceCollectionExtension
     /// <param name="serviceLifetime">Service Lifetime</param>
     public static void AddBlobClient(this IServiceCollection services,Action<BlobStoreOptions> options,ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
     {
-        var blobStoreOptions = new BlobStoreOptions();
-        options.Invoke(blobStoreOptions);
-            
+        var blobStoreOptions = BlobStoreOptions(options);
+
         services.AddHttpClient(nameof(BlobClient), 
             client => client.BaseAddress = new Uri(blobStoreOptions.Url));
         services.AddHttpClient(nameof(ContainerClient),
             client => client.BaseAddress = new Uri(blobStoreOptions.Url));
-            
-        var blobClientServiceDescriptor =
-            new ServiceDescriptor(typeof(IBlobClient), typeof(BlobClient), serviceLifetime);
-        var containerClientServiceDescriptor =
-            new ServiceDescriptor(typeof(IContainerClient), typeof(ContainerClient), serviceLifetime);
-            
-        services.Add(blobClientServiceDescriptor);
-        services.Add(containerClientServiceDescriptor);
+        
+        services.Add(new ServiceDescriptor(typeof(IBlobClient), typeof(BlobClient), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(IContainerClient), typeof(ContainerClient), serviceLifetime));
+    }
+
+    /// <summary>
+    /// Gets <see cref="BlobStoreOptions"/> from <see cref="Action{T}"/> and returns.
+    /// </summary>
+    /// <param name="options"><see cref="Action{T}"/></param>
+    /// <returns><see cref="BlobStoreOptions"/></returns>
+    private static BlobStoreOptions BlobStoreOptions(Action<BlobStoreOptions> options)
+    {
+        var blobStoreOptions = new BlobStoreOptions();
+        options.Invoke(blobStoreOptions);
+        return blobStoreOptions;
     }
 }
 
